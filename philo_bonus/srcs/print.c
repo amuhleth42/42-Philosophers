1ec8fc5556524a3f7e940f63b8d69ce8a9a1cd3b
Aymeric Muhlethaler <96829299+amuhleth42@users.noreply.github.com>
2022-06-03 20:00:41 +0200

    Fix

--- philo_bonus/srcs/print.c
+++ philo_bonus/srcs/print.c
@@ -6,13 +6,32 @@
 /*   By: amuhleth <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/02 18:53:00 by amuhleth          #+#    #+#             */
-/*   Updated: 2022/06/03 19:20:25 by amuhleth         ###   ########.fr       */
+/*   Updated: 2022/06/03 19:30:47 by amuhleth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo_bonus.h"
 
 void	print_log(t_data *a, int cmd)
+{
+	sem_wait(a->writing);
+	a->timestamp = get_timestamp(a);
+	printf("%7d ms    philo nb %-3d ", a->timestamp, a->philo.nb + 1);
+	if (cmd == FORK)
+		printf("has taken a fork\033[0m\n");
+	else if (cmd == EAT)
+		printf("is eating\033[0m, %d\n", a->philo.meal_count + 1);
+	else if (cmd == SLEEP)
+		printf("is sleeping\033[0m\n");
+	else if (cmd == THINK)
+		printf("is thinking\033[0m\n");
+	else if (cmd == DIED)
+		printf("died\033[0m\n");
+	if (cmd != DIED)
+		sem_post(a->writing);
+}
+
+/*void	print_log(t_data *a, int cmd)
 {
 	sem_wait(a->writing);
 	a->timestamp = get_timestamp(a);
@@ -29,4 +48,4 @@ void	print_log(t_data *a, int cmd)
 		printf("\033[0;31mdied\033[0m\n");
 	if (cmd != DIED)
 		sem_post(a->writing);
-}
+}*/
